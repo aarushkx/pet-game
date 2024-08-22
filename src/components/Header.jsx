ecb0530516846bcf04c3c2003c20ce9d2441a2f7
Aarush Kumar <2005aarushkumar@gmail.com>
2024-08-23 01:26:36 +0530

    Change header text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
     return (
         <header className="min-w-full flex justify-center items-center text-center p-4 text-[1.5rem] text-[#457534] font-bold bg--[#d8e3d4]">
-            Happy to chirp alongside a new buddy!
+            Happy to chirp alongside my new buddy!
         </header>
     );
 }
